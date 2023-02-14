0577ec82909d1fcb88ac1e2e64f161279e0d7e4c
Shayan Rezvani <shayanrezvani5@gmail.com>
2023-02-14 22:18:18 +0330

    firstCommit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/' ,(req ,res) => {
+    res.sendFile(__dirname + '/index.html');
+
+})
+
+app.post('/' , (req ,res) =>{
+    const query = req.body.cityName
+    const apiKey = "452cbe682c3d98a3f2d29124eaec91d2"
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=`+query+'&appid='+apiKey+'&units=metric'
+    https.get( url , (response) => {
+        response.on('data' , (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const discription = weatherData.weather[0].description;
+            res.write("<h1>the Temperature in " + query + 'is' + temp + " degree celcius</h1>")
+            res.write('<p>the conditon of discription is ' + discription + '<p>')
+        })
+        
+    })
+})
+
+app.listen(3000 , () =>{
+    console.log('server is running at 3000');
+})
